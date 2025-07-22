b6811c3f219c53d0647983cd4c78df4be9238738
jahnavi-adabala <jahnavi.adabala@sasi.ac.in>
2025-07-22 11:24:30 +0530

    2 weeks completed

---
+++ basic.js
@@ -0,0 +1,6 @@
+        let name="Jahnavi";
+        let age= "20";
+        let isStudent="true";
+        console.log("Name is",name);
+        console.log("My age is",age);
+        console.log("Student",isStudent);
---
+++ day2.js
@@ -0,0 +1,2 @@
+let l="jahnavi"
+console.log("Hello "+l);
